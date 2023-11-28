26d54afed4bc3907819a814352d8a77cdc31ee9d
Polly <riviera.koder@gmail.com>
2023-11-28 13:50:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function submit() {
+    let newUser = document.querySelector("#textUsername").value;
+    let newEmail = document.querySelector("#textEmail").value;
+    let newPass = document.querySelector("#textPassword").value;
+    
+    let outputP = document.querySelector("#p");
+    outputP.textContent = newUser, newEmail, newPass;
+}
+
+let submitButton = document.querySelector("#submitButton");
+submitButton.onclick = submit();
