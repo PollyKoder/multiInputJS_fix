efc5be81cf45507e4d07b7b21a37e65b41c84b46
Polly <riviera.koder@gmail.com>
2023-11-29 11:18:41 +0100

    editFunction

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+const newUser = document.querySelector("#textUsername");
+const newEmail = document.querySelector("#textEmail");
+const newPass = document.querySelector("#textPassword");
+
+// newUser = [];
+// newEmail = [];
+// newPass = [];
+
+const submitButton = document.querySelector("#submitButton");
+const pUser = document.querySelector("#pUser1");
+const pEmail = document.querySelector("#pEmail2");
+const pPass = document.querySelector("#pPass3");
+
 function submit() {
-    let newUser = document.querySelector("#textUsername").value;
-    let newEmail = document.querySelector("#textEmail").value;
-    let newPass = document.querySelector("#textPassword").value;
-    
-    let outputP = document.querySelector("#p");
-    outputP.textContent = newUser;
-    // outputP.textContent = newEmail;
-    // outputP.textContent = newPass;
+    pUser.innerHTML = `Your registered username is: ${newUser.value}`;
+    pEmail.innerHTML = `Your registered email is: ${newEmail.value}`;
+    pPass.innerHTML = `Your registered password is: ${newPass.value}`;
 }
 
-let submitButton = document.querySelector("#submitButton");
-submitButton.onclick = submit();
+function output() {
+    pUser;
+    pEmail;
+    pPass;
+}
+
+submitButton.onclick = output;
